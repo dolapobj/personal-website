6c03206624c0e3a11a4dc4ca6604de8913d8be40
Dolapo Adedokun <adedokun@mit.edu>
2022-07-01 14:10:09 -0400

    deleted unecessary imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,8 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
-import Button from '../components/button'
+
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
