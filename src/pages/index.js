73401463d60edafe00f428f155d38c42037e5863
Dolapo Adedokun <adedokun@mit.edu>
2022-07-04 14:51:52 -0400

    adjusted fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
+import Karla1 from "../components/layout.css";
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -16,26 +16,23 @@ const IndexPage = () => {
   return ( (
   <Layout pageHeading="Home Page" pageTitle = "Home">
     <Seo title="Home" keywords={[`MIT`, `computer science`, `CS`]} />
-    <body
-    style={{
-      fontFamily: "sans-serif"
-      
-    }}>
+    <body>
     <div id='holder'>
         <div id='left'></div>
         <div id='middle'>
-            <h1 style={{ fontFamily: "sans-serif",paddingRight : '5px'}}>Hi! I'm Dolapo Adedokun.</h1>
+            <h1> Hi! I'm Dolapo Adedokun.</h1>
             <p>I'm a <a href="https://news.mit.edu/2021/adedolapo-adedokun-mitchell-scholar-1122"> Mitchell Scholar</a> at Trinity College Dublin and I studied CS at MIT. In the past I've worked on: </p>
             <ul>
                 <li>Nonce Features in Reference Games (<a href={data.file.publicURL}><em>paper</em></a>) </li>
-                <li>reinventing internal data management at Salesforce</li>
-                <li>internal tool development at Slack</li>
-                <li>improving data pipeline migration at Visa</li>
-                <li>reinforcement learning at the MIT Quest for Intelligence</li>
+                <li>Reinventing internal data management at Salesforce</li>
+                <li>Internal tool development at Slack</li>
+                <li>Improving data pipeline migration at Visa</li>
+                <li>Reinforcement learning at the MIT Quest for Intelligence</li>
             </ul>
-            I love jazz guitar, piano, singing, and learning Japanese. Connect with me!
+            I love gospel and jazz guitar, piano, singing, and learning Japanese. Connect with me!
         </div>
         <div id='right'></div>
+
     </div>
     </body>
 
