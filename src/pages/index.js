58dcb67577017fa07bb139f41384995138095d13
dolapobj <dolapobj@gmail.com>
2024-09-09 18:37:43 -0400

    added prof pic and some transitions

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import Karla1 from "../components/layout.css";
+import "../components/layout.css";
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -12,39 +12,39 @@ const IndexPage = () => {
     }
   }
   `)
-  console.log(data);
-  return ( (
-  <Layout pageHeading="Dolapo Adedokun - Home Page" pageTitle = "Dolapo Adedokun - Home">
-     <Seo
+
+  return (
+    <Layout pageHeading="Dolapo Adedokun - Home Page" pageTitle="Dolapo Adedokun - Home">
+      <Seo
         title="Dolapo Adedokun"
-        keywords={[`gatsby`, `dolapo adedokun`, `application`, `react`, `dolapo`,`adedokun`,`blog`,`MIT`,`jazz`, `cs`, `computer science`]}
+        keywords={[`gatsby`, `dolapo adedokun`, `application`, `react`, `dolapo`, `adedokun`, `blog`, `MIT`, `jazz`, `cs`, `computer science`]}
       />
-    <body>
-    <div id='holder'>
-        <div id='left'></div>
-        <div id='middle'>
+      <div id="black-cover"></div>
+      <div id="content">
+        
+        <div id="holder">
+          <div id="middle">
             <h1> Hi! I'm Dolapo Adedokun.</h1>
-            <p>I'm a <a href="https://news.mit.edu/2021/adedolapo-adedokun-mitchell-scholar-1122"> Mitchell Scholar</a> at Trinity College Dublin and I studied CS at MIT. </p>
-            <p>In the past I've worked on: </p>
+            <p>I'm a <a href="https://news.mit.edu/2021/adedolapo-adedokun-mitchell-scholar-1122"> Mitchell Scholar</a> at Trinity College Dublin and I studied CS at MIT.</p>
+            <p>In the past I've worked on:</p>
             <ul>
-                <li>Nonce Features in Reference Games (<a href={data.file.publicURL}><em>paper</em></a>) </li>
-                <li>Reinventing internal data management at Salesforce</li>
-                <li>Internal tool development at Slack</li>
-                <li>Improving data pipeline migration at Visa</li>
-                <li>Reinforcement learning at the MIT Quest for Intelligence</li>
-                <li> <a href= "https://www.exeter.edu/news/capturing-color"> Colored Seeds,</a> a photography project from 2018 </li>
+              <li>Nonce Features in Reference Games (<a href={data.file.publicURL}><em>paper</em></a>)</li>
+              <li>Reinventing internal data management at Salesforce</li>
+              <li>Internal tool development at Slack</li>
+              <li>Improving data pipeline migration at Visa</li>
+              <li>Reinforcement learning at the MIT Quest for Intelligence</li>
+              <li><a href="https://www.exeter.edu/news/capturing-color">Colored Seeds</a>, a photography project from 2018</li>
             </ul>
             I love gospel and jazz guitar, piano, singing, and learning Japanese. Connect with me!
+          </div>
+          <div id="right">
+            <img src="/thumb1.png" alt="Thumbnail"  />
+          </div>
         </div>
-        <div id='right'></div>
-
-    </div>
-    </body>
-
-  </Layout>
-
-)
+      </div>
+    </Layout>
   )
 }
-  
+
 export default IndexPage
+
